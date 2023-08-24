d08dd076e17bd04498a51cf6787ac9598f27bb4b
Cesar <cesaralvaromirandagutierrez1@gmail.com>
2023-08-23 22:56:09 -0400

    login terminado

--- GestionBiblioteca/conexion.php
+++ GestionBiblioteca/conexion.php
@@ -1,6 +1,6 @@
 <?php
 // Conexión a la base de datos local
-$con = mysqli_connect("localhost", "root", "", "bd_gbiblioteca");
+$con = mysqli_connect("localhost", "root", "", "bd_gestionbilbioteca");
 if ($con->connect_error){
     die ("No se pudo conectar con el servidor".$con->connect_error);
 }
--- GestionBiblioteca/login.php
+++ GestionBiblioteca/login.php
@@ -7,7 +7,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $password = $_POST['password'];
 
     // Consulta para obtener los valores de admin y password
-    $query = "SELECT * FROM trabajadores WHERE nombres=$username AND contrasena=$password";
+    $query = "SELECT * FROM trabajadores WHERE nombres='$username' AND contrasena='$password'";
     $result = mysqli_query($con, $query);
         // Comprobar si se encontró al usuario admin
     if (mysqli_num_rows($result) == 1) {
@@ -37,10 +37,10 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     // Comprobar si el nombre de usuario y la contraseña son correctos y redirigir a la pagina adecuada.
     if ($username == $nombre && $password == $contrasena && $nivel==1) {
         //header('location: index.php');
-        echo "ERES UN DAMIN JEJE, tu pagina web pronto sera creada!!"
+        echo "ERES UN DAMIN JEJE, tu pagina web pronto sera creada!!";
     } else if ($username == $nombre && $password == $contrasena && $nivel==2){
         //header('location: index.php');
-        echo "ERES MIENDRO DE LA EMPRESA JEJE, tu pagina web pronto sera creada!!"
+        echo "ERES MIENDRO DE LA EMPRESA JEJE, tu pagina web pronto sera creada!!";
     }else{
                 // Mostrar mensaje de error
                 echo "<p class='error'>Usuario o contraseña incorrectos</p>";
