b16ac2d071a86e8e65f3e0aa706225570013210d
Cesar <cesaralvaromirandagutierrez1@gmail.com>
2023-08-23 22:45:45 -0400

    login

--- GestionBiblioteca/conexion.php
+++ GestionBiblioteca/conexion.php
@@ -1,22 +1,7 @@
 <?php
 // Conexión a la base de datos local
-$db = mysqli_connect('localhost', 'username', 'password', 'database_name');
-
-// Consulta para obtener los valores de admin y password
-$query = "SELECT * FROM users WHERE username='admin'";
-$result = mysqli_query($db, $query);
-
-// Comprobar si se encontró al usuario admin
-if (mysqli_num_rows($result) == 1) {
-    // Obtener los valores de admin y password
-    $row = mysqli_fetch_assoc($result);
-    $admin_username = $row['username'];
-    $admin_password = $row['password'];
-
-    // Mostrar los valores de admin y password
-    echo "Admin username: $admin_username<br>";
-    echo "Admin password: $admin_password<br>";
-} else {
-    // Mostrar mensaje de error
-    echo "No se encontró al usuario admin";
+$con = mysqli_connect("localhost", "root", "", "bd_gbiblioteca");
+if ($con->connect_error){
+    die ("No se pudo conectar con el servidor".$con->connect_error);
 }
+?>
--- GestionBiblioteca/login.php
+++ GestionBiblioteca/login.php
@@ -1,18 +1,49 @@
 <?php
+include 'conexion.php';
+
 // Comprobar si el formulario ha sido enviado
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Comprobar si el nombre de usuario y la contraseña son correctos
-    if ($username == 'admin' && $password == 'password') {
-        // Iniciar sesión y redirigir a la página principal
+    // Consulta para obtener los valores de admin y password
+    $query = "SELECT * FROM trabajadores WHERE nombres=$username AND contrasena=$password";
+    $result = mysqli_query($con, $query);
+        // Comprobar si se encontró al usuario admin
+    if (mysqli_num_rows($result) == 1) {
+        // Obtener los valores de admin y password
+        $row = mysqli_fetch_assoc($result);
+        $nombre = $row['nombres'];
+        $contrasena = $row['contrasena'];
+        $nivel=$row['nivel'];
+        
+    } else {
+        if(mysqli_num_rows($result) > 1){
+        // Obtener los valores de admin y password
+        $row = mysqli_fetch_assoc($result);
+        $nombre = $row['nombres'];
+        $contrasena = $row['contrasena'];
+        $nivel=$row['nivel'];
+        // Mostrar mensaje de alerta
+        echo "ALERTA Hay mas de un usuario, cambie su contrasena";
+        }
+        // Mostrar mensaje de error
+        echo "el registro no esta completo";
+    }
+        // crear session con el nombre del usuario para su futuro uso
         session_start();
         $_SESSION['username'] = $username;
-        header('location: index.php');
-    } else {
-        // Mostrar mensaje de error
-        echo "<p class='error'>Usuario o contraseña incorrectos</p>";
+
+    // Comprobar si el nombre de usuario y la contraseña son correctos y redirigir a la pagina adecuada.
+    if ($username == $nombre && $password == $contrasena && $nivel==1) {
+        //header('location: index.php');
+        echo "ERES UN DAMIN JEJE, tu pagina web pronto sera creada!!"
+    } else if ($username == $nombre && $password == $contrasena && $nivel==2){
+        //header('location: index.php');
+        echo "ERES MIENDRO DE LA EMPRESA JEJE, tu pagina web pronto sera creada!!"
+    }else{
+                // Mostrar mensaje de error
+                echo "<p class='error'>Usuario o contraseña incorrectos</p>";
     }
 }
 ?>
